fd9e92efaf8d1ced0e2d3988979eb5b32e2198dd
JaimeGoB <josba93@gmail.com>
2021-05-12 13:04:19 -0500

    Created In Theater and Future Release in UI

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -5,26 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-
-    //Simulating a DB request
-    setTimeout(() => {
-      this.movies = [{
-        title: 'Spider-Man',
-        releaseDate: new Date(),
-        price: 1400.99
-      },
-      {
-        title: 'Moana',
-        releaseDate: new Date(),
-        price: 300.99
-      }];
-    },0) //Time to wait to execute function
-  }
-  title = 'any value';
-
-  movies;
+export class AppComponent {
 
   squareNumber(n: number){
     return n*n;
--- angular-movies/src/app/movies/movies-list/movies-list.component.ts
+++ angular-movies/src/app/movies/movies-list/movies-list.component.ts
@@ -10,6 +10,23 @@ export class MoviesListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    //Simulating a DB request
+    setTimeout(() => {
+      this.movies = [{
+        title: 'Spider-Man',
+        releaseDate: new Date(),
+        price: 1400.99
+      },
+      {
+        title: 'Moana',
+        releaseDate: new Date(),
+        price: 300.99
+      }];
+    },0) //Time to wait to execute function
   }
+  title = 'any value';
+
+  movies;
 
 }
