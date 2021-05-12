2dcf1179669f908927e362cb0f98f932339e4cad
JaimeGoB <josba93@gmail.com>
2021-05-11 21:03:49 -0500

    Adding loding image before simulating a loading data from db

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    //Simulating a DB request
+    setTimeout(() => {
+      this.movies = [{
+        title: 'Spider-Man',
+        releaseDate: new Date(),
+        price: 1400.99
+      },
+      {
+        title: 'Moana',
+        releaseDate: new Date(),
+        price: 300.99
+      }];
+    },2000) //Time to wait to execute function
+  }
   title = 'any value';
+
+  movies;
+
+  squareNumber(n: number){
+    return n*n;
+  }
 }
