173dff50a1e73b4035faa7cbc7158e755ac39416
JaimeGoB <josba93@gmail.com>
2021-05-17 14:49:18 -0500

    Displa movies with poster and good UI.

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -11,48 +11,14 @@ export class AppComponent implements OnInit {
     this.moviesInTheaters = [{
       title: 'Spider-Man',
       releaseDate: new Date(),
-      price: 1400.99
+      price: 1400.99,
+      poster: 'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
     },
     {
       title: 'Moana',
       releaseDate: new Date(),
-      price: 300.99
-    },{
-      title: 'Spider-Man',
-      releaseDate: new Date(),
-      price: 1400.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
-    },{
-      title: 'Spider-Man',
-      releaseDate: new Date(),
-      price: 1400.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
-    },{
-      title: 'Spider-Man',
-      releaseDate: new Date(),
-      price: 1400.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
-    },{
-      title: 'Spider-Man',
-      releaseDate: new Date(),
-      price: 1400.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
+      price: 300.99,
+      poster: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_UX182_CR0,0,182,268_AL_.jpg'
     }];
 
     this.moviesFutureReleases = [];
