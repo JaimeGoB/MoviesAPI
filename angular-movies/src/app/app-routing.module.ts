57d1b0cb6611877d577a4d690c8afe9502f759ca
JaimeGoB <josba93@gmail.com>
2021-05-17 23:23:36 -0500

    Added Genres button that will route to Genres Component in toolbar

--- angular-movies/src/app/app-routing.module.ts
+++ angular-movies/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
 import { HomeComponent } from './home/home.component';
 
 /* Configure routing rules - tell angular to display certain component from url */
 const routes: Routes = [
   //ROOT of Application  show home component
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'genres', component: IndexGenresComponent}
 ];
 
 @NgModule({
